309423a6f79de837d2fe5525fef0cf698db653a2
William Chou <williamchoudev@gmail.com>
2019-05-28 22:20:22 -0400

    Add spinner for better user experience

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyledSpinnerContainer, StyledSpinner } from './style';
+
+const Spinner = () => (
+  <StyledSpinnerContainer>
+    <StyledSpinner />
+  </StyledSpinnerContainer>
+);
+
+export default Spinner;
